199481ffa8a59c3e0e29f0f626ff38b455383141
AhmedHeikal <heikal1996@gmail.com>
2021-02-03 06:13:04 +0400

    First Online Submision

---
+++ README.md
@@ -0,0 +1,7 @@
+# personal_expenses_project - <NOT MY IDEA>
+
+It's made by following the instructions of Max in Flutter & Dart - The Complete Guide.
+
+[https://www.udemy.com/course/learn-flutter-dart-to-build-ios-android-apps/]
+
+This is the product of sections 4, 5 & 6.
---
+++ lib/widgets/new_transaction.dart
@@ -0,0 +1,112 @@
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../widgets/adaptive_button.dart';
+
+final _titleController = TextEditingController();
+final _amountController = TextEditingController();
+DateTime _selectedDate;
+
+class NewTransaction extends StatefulWidget {
+  final Function addTransacton;
+  NewTransaction(this.addTransacton);
+
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  void _submitData() {
+    if (_amountController.text.isEmpty) {
+      return;
+    }
+    final enteredTitle = _titleController.text;
+    final enteredAmount = double.parse(_amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0 || _selectedDate == null) {
+      return;
+    }
+
+    widget.addTransacton(
+      enteredTitle,
+      enteredAmount,
+      _selectedDate,
+    );
+    Navigator.of(context).pop();
+  }
+
+  void _percentDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2020),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Card(
+        elevation: 5,
+        child: Container(
+          padding: EdgeInsets.only(
+              top: 10,
+              left: 10,
+              right: 10,
+              bottom: MediaQuery.of(context).viewInsets.bottom + 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              TextField(
+                decoration: InputDecoration(labelText: 'Title'),
+                controller: _titleController,
+                onSubmitted: (_) => _submitData(),
+
+                // ONCHANGED: (VAL) {
+                //   TITLEINPUT = VAL;
+                // },
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'Amount'),
+                controller: _amountController,
+                keyboardType: TextInputType.number,
+                onSubmitted: (_) => _submitData(),
+                // onChanged: (val) => amountInput = val,
+              ),
+              Container(
+                height: 70,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Text(_selectedDate == null
+                          ? 'No date Chosen!'
+                          : 'Picked Date:${DateFormat.yMd().format(_selectedDate)}'),
+                    ),
+                    AdaptiveFlatButton('Choose Date', _percentDatePicker),
+                  ],
+                ),
+              ),
+              RaisedButton(
+                child: Text(
+                  'Add Transaction',
+                ),
+                onPressed: _submitData,
+                textColor: Theme.of(context).textTheme.button.color,
+                color: Theme.of(context).primaryColor,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
